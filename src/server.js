2d1013e6ed6852e249903ea22147e84fae26973d
albertocgcarriongonzalez <albertocgcarriongonzalez@gmail.com>
2022-08-18 01:17:24 -0500

    actualizaciones para heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/heladeria-xtasis-tropical2'));
+app.use(express.static(__dirname+'/dist/ng-blog'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/heladeria-xtasis-tropical2/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
